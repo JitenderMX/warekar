d11e6ed248c81543f3eb6c6e3ea41df24dc6fb7e
JitenderMX <jitender.work.mediax@gmail.com>
2024-01-30 15:24:31 +0530

    project page remove project with no data

--- send_email.php
+++ send_email.php
@@ -5,7 +5,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $message = $_POST["message"];
 
-    $to = "jitender.work.mediax@gmail.com"; // Replace with your email address
+    $to = "info@mrwapl.com"; // Replace with your email address
     $subject = "Contact Form Submission";
     $body = "Full Name: $full_name\nPhone: $phone\nEmail: $email\nMessage:\n$message";
 
